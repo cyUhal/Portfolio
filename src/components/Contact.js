9e0db18ce9b60fd9b4dc3da41ecf1676c0f70b21
cyUhal <cyntiauhalde22@gmail.com>
2023-12-20 13:15:50 +0000

    email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,16 +5,16 @@ import React from 'react'
     <div className='page'>
       <h1 className='heading'>Contacto</h1>
 
-      <form className='contact' action='mailto: cyntiauhalde22@gmail'>
+     {/*  <form className='contact' action='mailto: cyntiauhalde22@gmail'>
         <input type='text' placeholder='name' className='input' />
         <input type='text' placeholder='Last Name' className='input'/>
         <input type='text' placeholder='e-mail' className='input' />
         <textarea placeholder='Reason for contact' className='input' />
         <input  type='submit' value='Enviar' className='submit' />
 
-      </form>
+      </form> */}
       
-      
+      <h2 className='heading' > cyntiauhalde22@gmail.com </h2>
     </div>
   )
 }
